b0d3dfd5eaf79f585e1753668700afc9c6a11be7
hanjoo0211 <hanjoo0211@gmail.com>
2025-07-06 18:08:40 +0900

    Basics - Packages

---
+++ 004_imports.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Printf("Wow~~ %f %g\n", 7.12, 7.12)
+}
---
+++ 005_exported-names.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Pi)
+	fmt.Printf("%g %f\n", math.Pi, math.Pi)
+}
